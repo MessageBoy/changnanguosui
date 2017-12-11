048e0decf2cd09e2202387f24d043026229b436d
Gaoshang1 <2452788192@qq.com>
2017-12-11 22:20:01 +0800

    12.11

---
+++ app/src/main/java/com/outsource/changnanguoshui/fragment/ActivityDetailsFragment.java
@@ -0,0 +1,32 @@
+package com.outsource.changnanguoshui.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.outsource.changnanguoshui.R;
+import com.outsource.changnanguoshui.application.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/12/10.
+ */
+
+public class ActivityDetailsFragment extends BaseFragment
+{
+    @Override
+    protected void initView(View view, Bundle savedInstanceState)
+    {
+
+    }
+
+    @Override
+    protected int getLayoutId()
+    {
+        return R.layout.fragment_activity_details;
+    }
+
+    @Override
+    protected void initData()
+    {
+
+    }
+}
