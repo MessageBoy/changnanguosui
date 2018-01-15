020ac19658ef77a61108f8c1c3e4b306ebd9a8cd
Gaoshang1 <2452788192@qq.com>
2018-01-15 20:12:14 +0800

    01/15

--- app/src/main/java/com/outsource/changnanguoshui/activity/StudyDetailsActivity.java
+++ app/src/main/java/com/outsource/changnanguoshui/activity/StudyDetailsActivity.java
@@ -1,8 +1,9 @@
 package com.outsource.changnanguoshui.activity;
 
-import android.util.Log;
 import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,7 +11,6 @@ import com.outsource.changnanguoshui.Constant;
 import com.outsource.changnanguoshui.R;
 import com.outsource.changnanguoshui.application.BaseActivity;
 import com.outsource.changnanguoshui.utlis.SpUtils;
-import com.outsource.changnanguoshui.utlis.WebUtils;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -39,10 +39,28 @@ public class StudyDetailsActivity extends BaseActivity
     protected void initData()
     {
         title.setText(getIntent().getStringExtra("activityTitle"));
-        WebUtils.webSetting(webView, this);
+//        WebUtils.webSetting(webView, this);
         String webUrl = getIntent().getStringExtra("webUrl");
-        Log.e("webUrl", webUrl);
+//        Log.e("webUrl", webUrl);
+//        webView.loadUrl(webUrl + "&user_id=" + SpUtils.getParam(this, Constant.USER_ID, ""));
+
+
+        WebSettings webSettings = webView.getSettings();
+        // 设置WebView属性，能够执行Javascript脚本
+        webSettings.setJavaScriptEnabled(true);
+        // 设置可以访问文件
+        webSettings.setAllowFileAccess(true);
+        // 设置文本编码
+        webSettings.setDefaultTextEncodingName("GBK");
+        // 适应屏幕，内容将自动缩放
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        // 设置缓存模式
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
         webView.loadUrl(webUrl + "&user_id=" + SpUtils.getParam(this, Constant.USER_ID, ""));
+        webView.setWebViewClient(new webViewClient());
+
     }
 
     @OnClick(R.id.back)
@@ -51,17 +69,28 @@ public class StudyDetailsActivity extends BaseActivity
         finish();
     }
 
-    public boolean onKeyDown(int keyCode, KeyEvent keyEvent)
+    @Override
+    // 设置回退
+    // 覆盖Activity类的onKeyDown(int keyCoder,KeyEvent event)方法
+    public boolean onKeyDown(int keyCode, KeyEvent event)
     {
-        if (keyCode == keyEvent.KEYCODE_BACK)
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack())
         {
-            if (webView.canGoBack())
-            {
-                webView.goBack();
-                return true;
-            }
+            webView.goBack(); // goBack()表示返回WebView的上一页面
+            return true;
         }
-        return super.onKeyDown(keyCode, keyEvent);
 
+        finish();// 结束退出程序
+        return false;
+    }
+
+    // Web视图
+    private class webViewClient extends WebViewClient
+    {
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
