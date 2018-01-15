16e3dd86aa80fdcda7da159abe7d872eb25b4235
MessageWhite <2396790411@qq.com>
2018-01-15 20:25:31 +0800

    1.15

--- app/src/main/java/com/outsource/changnanguoshui/LoginActivity.java
+++ app/src/main/java/com/outsource/changnanguoshui/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.outsource.changnanguoshui;
 
+import android.os.Handler;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,9 +13,15 @@ import com.outsource.changnanguoshui.utlis.JsonGenerics;
 import com.outsource.changnanguoshui.utlis.SpUtils;
 import com.zhy.http.okhttp.OkHttpUtils;
 
+import java.util.Set;
+
 import butterknife.BindView;
 import butterknife.OnClick;
+import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
 import okhttp3.Call;
+
+import static com.outsource.changnanguoshui.Constant.TOKEN;
 
 public class LoginActivity extends BaseActivity
 {
@@ -25,7 +33,8 @@ public class LoginActivity extends BaseActivity
     EditText userName;
     @BindView(R.id.user_pwd)
     EditText userPwd;
-
+    private static final int MSG_SET_ALIAS = 1001;
+    private String TAG = MainActivity.class.getName();
     @Override
     protected void initView()
     {
@@ -80,9 +89,11 @@ public class LoginActivity extends BaseActivity
                     {
                         if (response.getStatus() == 1)
                         {
-                            SpUtils.setParam(LoginActivity.this, Constant.TOKEN, response.getToken());
+                            SpUtils.setParam(LoginActivity.this, TOKEN, response.getToken());
                             SpUtils.setParam(LoginActivity.this, Constant.USER_ID, response.getUser_id());
                             SpUtils.setParam(LoginActivity.this, Constant.IsLogin, true);
+                            //注册极光推送的别名
+                            mHandler.sendMessage(mHandler.obtainMessage(MSG_SET_ALIAS, response.getUser_id().toString()));
                             startActivity(MainActivity.class);
                             finish();
                         } else
@@ -91,8 +102,43 @@ public class LoginActivity extends BaseActivity
                         }
                     }
                 });
-
-
     }
 
+    private final Handler mHandler = new Handler()
+    {//专门用了一个Handler对象处理别名的注册问题
+        @Override
+        public void handleMessage(android.os.Message msg)
+        {
+            super.handleMessage(msg);
+            Log.d(TAG, "设置极光推送的别名-mHandler2");
+            JPushInterface.setAliasAndTags(getApplicationContext(),
+                    (String) msg.obj, null, mAliasCallback);
+        }
+    };
+
+    //注册别名回调
+    private final TagAliasCallback mAliasCallback = new TagAliasCallback()
+    {
+        @Override
+        public void gotResult(int code, String alias, Set<String> tags)
+        {
+            String logs;
+            switch (code)
+            {
+                case 0:
+                    logs = "极光推送别名设置成功";
+                    Log.i(TAG, logs);
+                    break;
+                case 6002:
+                    logs = "极光推送别名设置失败，60秒后重试";
+                    Log.i(TAG, logs);
+                    mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_SET_ALIAS, alias), 1000 * 60);
+                    break;
+                default:
+                    logs = "极光推送设置失败，Failed with errorCode = " + code;
+                    Log.e(TAG, logs);
+                    break;
+            }
+        }
+    };
 }
